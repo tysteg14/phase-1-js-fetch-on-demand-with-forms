c22ef7d76757fcbef61a41353a860469367e8384
Tyrell <tysteg14@gmail.com>
2023-08-25 10:55:11 -0500

    fetch on demand

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('input#searchByID')
+
+    console.log(input.value);
+
+    const movieURL = 'http://localhost:3000/movies'
+
+    fetch(`${movieURL}/${input.value}`)
+       .then((response) => response.json())
+       .then((data) => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+
+        inputForm.reset()
+       });
+  })
+};
 
 document.addEventListener('DOMContentLoaded', init);
